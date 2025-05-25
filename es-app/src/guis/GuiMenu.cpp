a3febc12386feaa5316455bf6d32a675a2967ad0
worstcase-scenario <iamspamalot@gmx.de>
2025-05-25 15:43:36 +0200

    working menu entry

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -964,12 +964,12 @@ void GuiMenu::createGamepadConfig(Window* window, GuiSettings* systemConfigurati
 	gamepadConfiguration->addEntry(_("ACTIVATE WIIMOTE WITH IR-SENSORBAR"), false, [window] {
     int result = system("/usr/bin/runwiimote.sh &");
     if(result == 0)
-        window->pushGui(new GuiMsgBox(window, _("Wiimote IR activated."), _("OK")));
+        window->pushGui(new GuiMsgBox(window, _("Wiimote activated."), _("OK")));
     else
         window->pushGui(new GuiMsgBox(window, _("Error while running script."), _("OK")));
 });
 
-});
+
 
 
 
