d6bbed7d9d39ee1f8b2ab42929187551061a40e7
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2025-09-11 23:22:21 +0200

    Use bit masks for GuiFileBrowser types

--- es-app/src/guis/GuiFileBrowser.h
+++ es-app/src/guis/GuiFileBrowser.h
@@ -10,15 +10,15 @@ class OptionListComponent;
 class GuiFileBrowser : public GuiComponent
 {
 public:
-	enum FileTypes
-	{
-		IMAGES = 1,
-		MANUALS = 2,
-		VIDEO = 3,
-		DIRECTORY = 4,
-		AUDIO = 5,
-		ALL = 255
-	};
+        enum FileTypes
+        {
+               IMAGES    = 1 << 0,
+               MANUALS   = 1 << 1,
+               VIDEO     = 1 << 2,
+               DIRECTORY = 1 << 3,
+               AUDIO     = 1 << 4,
+               ALL       = 0xFF
+        };
 
 	GuiFileBrowser(Window* window, const std::string startPath, const std::string selectedFile, FileTypes types = FileTypes::IMAGES, const std::function<void(const std::string&)>& okCallback = nullptr, const std::string& title = "");
 
