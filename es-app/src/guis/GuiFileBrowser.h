b11d3b60071068fe02b3cfb5ad3262b97e392acc
worstcase-scenario <83265453+worstcase-scenario@users.noreply.github.com>
2025-09-14 17:56:00 +0200

    Remove loading spinner from video previews

--- es-app/src/guis/GuiFileBrowser.h
+++ es-app/src/guis/GuiFileBrowser.h
@@ -7,37 +7,54 @@
 template<typename T>
 class OptionListComponent;
 
+#include <memory>
 class ImageComponent;
+class TextureResource;
+#include <vector>
 
 class GuiFileBrowser : public GuiComponent
 {
 public:
-	enum FileTypes
-	{
-		IMAGES = 1,
-		MANUALS = 2,
-		VIDEO = 3,
-		DIRECTORY = 4,
-		AUDIO = 5,
-		ALL = 255
-	};
+       enum FileTypes
+       {
+               IMAGES     = 1 << 0,
+               MANUALS    = 1 << 1,
+               VIDEO      = 1 << 2,
+               DIRECTORY  = 1 << 3,
+               AUDIO      = 1 << 4,
+               ALL        = 0xFFFFFFFF
+       };
 
-	GuiFileBrowser(Window* window, const std::string startPath, const std::string selectedFile, FileTypes types = FileTypes::IMAGES, const std::function<void(const std::string&)>& okCallback = nullptr, const std::string& title = "");
+        GuiFileBrowser(Window* window, const std::string startPath, const std::string selectedFile, FileTypes types = FileTypes::IMAGES, const std::function<void(const std::string&)>& okCallback = nullptr, const std::string& title = "");
+        ~GuiFileBrowser() override;
 
-	bool input(InputConfig* config, Input input) override;
-	virtual std::vector<HelpPrompt> getHelpPrompts() override;
+        bool input(InputConfig* config, Input input) override;
+        void update(int deltaTime) override;
+        virtual std::vector<HelpPrompt> getHelpPrompts() override;
 
 private:
         void onOk(const std::string& path);
         void navigateTo(const std::string path);
         void centerWindow();
+        void generateVideoPreview(const std::string& path);
+        void clearVideoPreview();
 
         MenuComponent mMenu;
 
         std::string mCurrentPath;
         std::string mSelectedFile;
-        FileTypes   mTypes;
-        std::shared_ptr<ImageComponent> mPreview;
+       FileTypes   mTypes;
+       std::shared_ptr<ImageComponent> mPreview;
+       std::shared_ptr<ImageComponent> mLoadingBg;
+       std::vector<std::string> mVideoFrames;
+       std::vector<std::shared_ptr<TextureResource>> mFrameTextures;
+        int mCurrentFrame;
+        int mFrameTime;
+        std::string mTempPreviewDir;
+       bool mGeneratingPreview;
+       int mExpectedFrames;
+       int mLastFrameCount;
+       int mNoFrameTime;
 
         std::function<void(const std::string&)> mOkCallback;
 };
